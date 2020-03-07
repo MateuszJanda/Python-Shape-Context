22db44930860720b7dd604f0aa6fa160fa204eb2
Mateusz Janda <mateusz.janda@gmail.com>
2020-03-07 20:35:03 +0100

    Total cost of test data

--- munkres.py
+++ munkres.py
@@ -831,18 +831,21 @@ if __name__ == '__main__':
        [0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2],
        [0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2]], dtype='float32')
 
-    cost_matrix = cost_matrix.tolist()
 
     # Print example cost matrix in C++ syntax
-    test_cost_matrix, _ = matrices[0]
+    test_cost_matrix = cost_matrix.tolist()
+    # test_cost_matrix, _ = matrices[0]
     test_values = "".join([str(val) + ", " for val in np.array(test_cost_matrix).flatten().tolist()[:-1]])
     test_values += str(np.array(test_cost_matrix).flatten().tolist()[-1])
     print("cv::Mat testCostMat = (cv::Mat_<int>(3, 3) << " + test_values + ");")
 
     m = Munkres()
-    # result = m.compute(cost_matrix)
-    result = m.compute(test_cost_matrix)
+    indexes = m.compute(test_cost_matrix)
+    # print(indexes)
 
-    print(result)
-
-
+    total_cost = 0
+    for r, c in indexes:
+        x = cost_matrix[r][c]
+        total_cost += x
+        print('(%d, %d) -> %f' % (r, c, x))
+    print('total_cost=%f' % total_cost)
